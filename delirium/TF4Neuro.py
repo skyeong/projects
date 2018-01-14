bb6197e1c4d697c1d60d754d02601c24e1c576b0
Sunghyon Kyeong <skyeong@Sunghyons-MacBook-Pro.local>
2018-01-14 18:48:04 +0900

    updated

--- delirium/TF4Neuro.py
+++ delirium/TF4Neuro.py
@@ -16,40 +16,62 @@ class TF4Neuro:
         print ('TF4Neuro is created.')
         self.df = DataFrame()
         self.edges = list()
-        self.learn_rate = 0.001
-        self.nNodes = 5
+        self.learn_rate = 0.0001
+        self.nNodes = 10
 
     def insert_data(self,fn_csv):
         #fn_csv = os.path.join(datapath,filename)
-        self.df = pd.read_csv(fn_csv, sep=',', header=0)
+        df = pd.read_csv(fn_csv, sep=',', header=0)
+        self.df = df.reindex(np.random.permutation(df.index)) # randomizing dataset
         print('shape = (%d, %d)' % self.df.shape)
 
-    def set_train_x(self, edges):
+    def set_train_x(self, edges, filter_key=None, filter_value=None):
         self.nFeatures = len(edges)
         self.edges = edges
-        self.train_x = self.df[edges].as_matrix()
+        if filter_key is not None:
+            df1 = self.df[self.df[filter_key]==filter_value]
+        else:
+            df1 = self.df
+        self.train_x = df1[edges].as_matrix()
 
-    def set_train_t(self,targetTag):
-        self.train_t = self.df[targetTag].as_matrix().reshape([len(self.df),1])
+    def set_train_t(self,targetTag,filter_key=None, filter_value=None):
+        if filter_key is not None:
+            df1 = self.df[self.df[filter_key]==filter_value]
+        else:
+            df1 = self.df
+        self.train_t = df1[targetTag].as_matrix().reshape([len(df1),1])
+
+    def set_test_x(self, edges, filter_key=None, filter_value=None):
+        self.nFeatures = len(edges)
+        self.edges = edges
+        if filter_key is not None:
+            df1 = self.df[self.df[filter_key]==filter_value]
+        else:
+            df1 = self.df
+        self.test_x = df1[edges].as_matrix()
+
+    def set_test_t(self,targetTag,filter_key=None, filter_value=None):
+        if filter_key is not None:
+            df1 = self.df[self.df[filter_key]==filter_value]
+        else:
+            df1 = self.df
+        self.test_t = df1[targetTag].as_matrix().reshape([len(df1),1])
 
     def set_num_nodes(self,nNodes):
         self.nNodes = nNodes
 
-    def prepare_singlelayer_session(self,mode='sigmoid'):
-        mult = self.train_x.flatten().mean()
+    def prepare_singlelayer_session(self):
         self.x = tf.placeholder(tf.float32, [None, self.nFeatures])
+       
+        # Parameters in Hidden layer
         w1 = tf.Variable(tf.truncated_normal([self.nFeatures, self.nNodes]))  # initialize using random normal distribution
         b1 = tf.Variable(tf.truncated_normal([self.nNodes]))
-        hidden1 = tf.nn.tanh(tf.matmul(self.x,w1) + b1*mult)  # *mult is to boost speed
+        hidden1 = tf.nn.sigmoid(tf.matmul(self.x,w1) + b1)  # *mult is to boost speed
 
+        # Parameters in Output layer
         w0 = tf.Variable(tf.zeros([self.nNodes, 1]))
         b0 = tf.Variable(tf.zeros([1]))
-        if mode is 'sigmoid':
-            p = tf.nn.softmax(tf.matmul(hidden1,w0)+b0*mult)
-        elif mode is 'tanh':
-            p = tf.nn.sigmoid(tf.matmul(hidden1,w0)+b0*mult)
-        elif mode is 'relu':
-            p = tf.nn.relu(tf.matmul(hidden1,w0)+b0*mult)
+        p = tf.nn.sigmoid(tf.matmul(hidden1,w0)+b0)
 
         # Error function
         self.t = tf.placeholder(tf.float32, [None,1])
@@ -75,11 +97,19 @@ class TF4Neuro:
         loss     = self.loss
         accuracy = self.accuracy
 
+        # for i,ii in enumerate(range(niter)):
+        #     sess.run(self.train_step, feed_dict={x:batch_x, t:batch_t})
+        #     if (i%(niter/10) == 0):
+        #         loss_val, acc_val = sess.run([loss, accuracy], feed_dict={x:train_x, t:train_t})
+        #         print('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val) )
+        datapoint_size = len(train_x)
+        batch_size = 20
         for i,ii in enumerate(range(niter)):
             sess.run(self.train_step, feed_dict={x:train_x, t:train_t})
             if (i%(niter/10) == 0):
-                loss_val, acc_val = sess.run([loss, accuracy], feed_dict={x:train_x, t:train_t})
+                loss_val, acc_val = sess.run([loss, accuracy], feed_dict={x:self.test_x, t:self.test_t})
                 print('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val) )
+
 
 
 if __name__=="__main__":
@@ -90,17 +120,18 @@ if __name__=="__main__":
             varName = 'r%d_%d'%(i+1,j+1)
             edges.append(varName)
 
-    nNodes = 5  # define number of nodes in hidden layer
-    niter = 50000
+    nNodes = 2  # define number of nodes in hidden layer
+    niter = 100000
 
     # Create TensorFlow Class
     neuro = TF4Neuro()
     neuro.insert_data('/Users/skyeong/data/delirium/Results/delirium_network_n116.csv')
-    #neuro.set_train_x(edges)
-    neuro.set_train_x(['r2_108','r4_92','r10_25','r10_26','r28_116','r32_60'])
-    neuro.set_train_t('Dx')
+    neuro.set_train_x(['r2_108','r4_92','r10_25','r10_26','r28_116','r32_60'],filter_key='expYear',filter_value=2014)
+    neuro.set_train_t('Dx',filter_key='expYear',filter_value=2014)
+    neuro.set_test_x(['r2_108','r4_92','r10_25','r10_26','r28_116','r32_60'],filter_key='expYear',filter_value=2008)
+    neuro.set_test_t('Dx',filter_key='expYear',filter_value=2008)
     neuro.set_num_nodes(nNodes)
-    neuro.prepare_singlelayer_session('tanh')
+    neuro.prepare_singlelayer_session()
     neuro.run_training_step(niter)
 
    
